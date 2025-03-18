8b98783bffeeb4ffa4e1eae82a1a8947363c2240
alenashmelts <alenashmelts@yandex.ru>
2025-03-18 10:29:10 +0500

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  darkMode:'selector',
+  theme: {
+    
+    extend: {
+      colors:{
+      'custome-green':'#396e47',
+      'my-red': '#d494c0',
+    },
+  },
+  },
+  plugins: [],
+}
+
